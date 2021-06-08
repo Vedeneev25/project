063314d5f6621974ccb22e3d3dee146fa144e180
Vedeneev25 <79744195+Vedeneev25@users.noreply.github.com>
2021-06-08 10:55:20 +0300

    Add files via upload

--- project.py
+++ project.py
@@ -2,12 +2,20 @@
 import openpyxl
 import cv2
 import pytesseract
+from tkinter import *
+'''
+pytesseract.pytesseract.tesseract_cmd = r'/Library/Frameworks/Python.framework/Versions/3.9/bin'
 img = cv2.imread('photo.png')
-#text = pytesseract.image_to_string(img)
-#img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-#config = r'--oem 3 --psm 6
-#cv2.imshow('Result', img)
-#cv2.waitKey(0)
+# to see the image below codes are there
+cv2.imshow('sampleimage',img)
+#enter any key to destroy the image window opened due to previous line code
+cv2.waitKey(1)
+cv2.destroyAllWindows()
+#convert image to text using tesseract
+text = pytesseract.image_to_string(img)
+print(text)
+'''
+
 
 #Открытие файла с себестоимотсью
 wb_obj = openpyxl.load_workbook("cena.xlsx")
@@ -20,18 +28,27 @@ napitki = ['cola','fanta','water1','water2','water3','coffee1','coffee2','coffee
 food = ['sandwich','cookie','laysbig','layssmall','чипсы','сэндвич','печенье']
 coffee = ['капучино','латте','раф']
 cola = [ 'cola' , 'coke' , 'cocacola','кола','кокакола','колаж/б0,5']
-# спросим у пользователя значение наценки которое он считает критичным
-krit = input("какое значение наценки в процентах для вас критично?").replace(' ', '')
-kritlist=list(krit)
-vvv=''
-for i, letter in enumerate(kritlist):
-    if letter in '0123456789':
-        vvv = vvv + str(letter)
-    else:
-        i = i + 1
-        
-big = float(vvv)
+i=1
+j=1
+n=(sheet_obj.max_row) 
+b=(sheet_obj1.max_row)
+n = n + 1
+b = b + 1
+row = 0
+window = Tk()
+window.title("Значение наценки")
+#lbl = Label(window, text=('Добрый день. Введите максимально допустимое значение наценки '), font=("Arial", 24))
+#lbl.grid(column=0, row=row)
+row = row + 1
 
+# спросим у пользователя значение наценки которое он считает критичным
+'''
+krit = Entry(window, width = 10)
+krit.grid(column=1, row=0)
+kritik = krit.get()
+'''
+fff = input('fff')
+big=float(fff)
 i=1
 j=1
 n=(sheet_obj.max_row) 
@@ -51,9 +68,8 @@ sred_coffee = 0
 ssred_coffee = 0
 sch_coffee = 0
 
-# Определение наценки и выявление нарушений
 
-        
+# Определение наценки и выявление нарушений        
 while i < n and j<b:
     # товар с файла с сс
     tovar = sheet_obj.cell(row=j, column = 1)
@@ -67,7 +83,10 @@ while i < n and j<b:
         l = cenass.value
         d = (k/l-1)*100
         if d > big:
-            print('наценка на товар ' + str(tovar.value) + ' равна ' + str(d) )
+            lbl = Label(window, text='наценка на товар ' + str(tovar.value) + ' равна ' + str(d))
+            lbl.grid(column=0, row=row)
+            row = row + 1
+            #print('наценка на товар ' + str(tovar.value) + ' равна ' + str(d) )
         if tovar.value in napitki:
             srednap = srednap + d
             sch = sch + 1
@@ -91,10 +110,34 @@ while i < n and j<b:
 ssrednap = srednap / sch
 ssredfood = sredfood / schfood
 ssred_coffee = sred_coffee / sch_coffee
-print('средняя наценка на напитки составляет ' + str(ssrednap))
-print('средняя наценка на еду составляет ' + str(ssredfood))
-print('средняя наценка на кофе составляет ' + str(ssred_coffee))
-
+def clicked():
+    row1 = 0
+    window = Tk()
+    window.title("Значение наценки")
+    lbl = Label(window, text=('средняя наценка на напитки составляет ' + str(ssrednap)), font=("Arial", 24))
+    lbl.grid(column=0, row=row1)
+    row1 = row1 + 1
+    lbl = Label(window, text=('средняя наценка на еду составляет ' + str(ssredfood)), font=("Arial", 24))
+    lbl.grid(column=0, row=row1)
+    row1 = row1 + 1
+    lbl = Label(window, text=('средняя наценка на кофе составляет ' + str(ssred_coffee)), font=("Arial", 24))
+    lbl.grid(column=0, row=row1)
+    row1 = row1 + 1
+    '''
+    res = "Привет {}".format(krit.get())
+    lbl = Label(window, text=(res), font=("Arial", 24))
+    lbl.grid(column=0, row=row1)
+    row1 = row1 + 1
+    lbl = Label(window, text=(kritik), font=("Arial", 24))
+    lbl.grid(column=0, row=row1)
+    row1 = row1 + 1
+    '''
+btn = Button(window, text="рассчитать среднее по категориям", font=("Arial Bold", 24), command=clicked)
+btn.grid(column=0, row=row)
+row = row + 1
+#print('средняя наценка на напитки составляет ' + str(ssrednap))
+#print('средняя наценка на еду составляет ' + str(ssredfood))
+#print('средняя наценка на кофе составляет ' + str(ssred_coffee))
 
 
 
